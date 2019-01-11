9228c2ea0f880195783d376415998e148578ef99
Varunaditya Jadwal <varunaditya93@gmail.com>
2019-01-11 20:11:37 +0530

    the MongoDB searches are now case insensitive

--- searchEngine/app.py
+++ searchEngine/app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask import request
 from flask_pymongo import PyMongo
 from flask import jsonify
-from bson.json_util import dumps
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'searchEngine'
@@ -11,28 +10,11 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/searchEngine'
 mongo = PyMongo(app)
 
 
-def debug(data):
-    print('*' * 1000)
-    print(data)
-    print('*' * 1000)
-
-
-# @app.route('/search', methods=['GET'])
-# def search():
-#     retDocuments = ''
-#     data = request.args['title']
-#     debug(data)
-#     myQuery = {'title': {'$regex': data}}
-#     collection = mongo.db.queries
-#     for i in collection.find(myQuery):
-#         # for i in collection.find({'_id': 0}):
-#         retDocuments += '{}-{}'.format(str(i['title']), str(i['content'])) + '|'
-#     return str(retDocuments)
-
 @app.route('/search', methods=['GET'])
 def search():
     data = request.args["title"]
-    myQuery = {'title': {'$regex': data}}
+    myQuery = {'title': {'$regex': data,
+                         '$options': 'i'}}
     collection = mongo.db.queries
     retDocuments = [i for i in collection.find(myQuery, {'_id': 0})]
     return jsonify(retDocuments)
